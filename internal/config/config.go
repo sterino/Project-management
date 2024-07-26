2a7c5c075a7eda051cfb7092785f10203b2d2886
sterino <egore.chip@gmail.com>
2024-07-26 17:08:10 +0500

    fix config and docker

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,41 +1,36 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"os"
-	"path/filepath"
 )
 
 type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+	DBHost     string `envconfig:"DBHost"`
+	DBPort     string `envconfig:"DBPort"`
+	DBUser     string `envconfig:"DBUser"`
+	DBPassword string `envconfig:"DBPassword"`
+	DBName     string `envconfig:"DBName"`
 }
 
 func LoadConfig() (cfg Config, err error) {
-	root, err := os.Getwd()
-	if err != nil {
-		return
-	}
-
-	err = godotenv.Load(filepath.Join(root, ".env"))
-	if err != nil {
-		cfg = Config{
-			DBHost:     os.Getenv("DBHost"),
-			DBPort:     os.Getenv("DBPort"),
-			DBUser:     os.Getenv("DBUser"),
-			DBPassword: os.Getenv("DBPassword"),
-			DBName:     os.Getenv("DBName"),
-		}
-		return
-	}
 
 	if err = envconfig.Process("DB", &cfg); err != nil {
 		return
 	}
-
+	//root, err := os.Getwd()
+	//if err != nil {
+	//	return
+	//}
+	//
+	//err = godotenv.Load(filepath.Join(root, ".env"))
+	//if err != nil {
+	//	return
+	//}
+	cfg.DBHost = os.Getenv("DBHost")
+	cfg.DBPort = os.Getenv("DBPort")
+	cfg.DBUser = os.Getenv("DBUser")
+	cfg.DBPassword = os.Getenv("DBPassword")
+	cfg.DBName = os.Getenv("DBName")
 	return
 }
